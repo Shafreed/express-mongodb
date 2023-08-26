d9ec2089ca4099ad96c0a588a36fc11d1795245f
Shafreed <shafsshafreedh321@gmail.com>
2023-08-26 12:16:02 +0530

    first name

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,11 @@ const app=express()
 const dotenv=require('dotenv').config()
 const {errorHandler}=require ('../BackEnd/middleware/errorMiddleware')
 const port= process.env.PORT || 5000;
+const connectDB=require ('./config/Db')
 
+
+
+connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
